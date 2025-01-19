dc120d3fe8d9b1ba20b2b2941c88f64bc955ac7a
Hansu Kim <cpm0722@gmail.com>
2025-01-19 15:25:24 +0900

    fix already installed NVM_DIR, node path

--- install.sh
+++ install.sh
@@ -120,13 +120,18 @@ if [[ ${OS_TYPE} == "UBUNTU" ]]; then
             apt autoremove -y
         fi
     fi
+    if [[ -n ${NVM_DIR} ]]; then
+        rm -rf ${NVM_DIR}
+        export -n NVM_DIR
+    fi
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-    nvm install ${NODE_MAJOR_VERSION}
     # download and install Node.js (you may need to restart the terminal)
     nvm install ${NODE_MAJOR_VERSION}
+    export PATH=/usr/local/nvm/versions/node/*/bin/node:${PATH}
+    echo 'export PATH=/usr/local/nvm/versions/node/*/bin/node:${PATH}' >> ${HOME}/.bashrc
 elif [[ ${OS_TYPE} == "MAC" ]]; then
     ${INSTALLER} install ${INSTALLER_OPTION} node@${NODE_MAJOR_VERSION}
 fi
