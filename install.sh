16a3c93b0993f3dd6970058275bf4ee48f1e5dd7
Hansu Kim <kim.hansu@navercorp.com>
2025-01-19 15:06:46 +0900

    update nvm found

--- install.sh
+++ install.sh
@@ -121,6 +121,10 @@ if [[ ${OS_TYPE} == "UBUNTU" ]]; then
         fi
     fi
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+    nvm install ${NODE_MAJOR_VERSION}
     # download and install Node.js (you may need to restart the terminal)
     nvm install ${NODE_MAJOR_VERSION}
 elif [[ ${OS_TYPE} == "MAC" ]]; then
